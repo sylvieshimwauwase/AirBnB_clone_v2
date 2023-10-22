21d663ff081013376a33d93e97868f0143deee08
sylvieshimwauwase <slvshimwa@gmail.com>
2023-10-23 00:09:31 +0200

    Web Frameworks

--- models/state.py
+++ models/state.py
@@ -8,29 +8,33 @@ from sqlalchemy import Column, Integer, String
 import models
 from models.city import City
 import shlex
+from os import getenv
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
+    """State class representation"""
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128),
+                nullable=False)
+        cities = relationship("City", cascade="all, delete",
+                backref="states")
+    else:
+        name = ""
 
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+    def __init__(self, *args, **kwargs):
+        """ initializing states"""
+        super().__init__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """ getter attribute"""
+            v_city = models.storage.all("City").values()
+            list_city = []
+            for city in v_city:
+                if city.state_id == self.id:
+                    list_city.append(city)
+            return list_city
